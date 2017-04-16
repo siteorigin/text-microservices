27e976eba7a7ed29b9dedcd693353f00068534da
Ubuntu <ubuntu@ip-172-30-0-162.ec2.internal>
2017-04-16 03:29:42 +0000

    process_skip-thought embedding to txt

--- process_src/process_skip-thought.py
+++ process_src/process_skip-thought.py
@@ -1,10 +1,13 @@
 
-
+import os
 import numpy as np
-m = np.load('embeddings.npy')
-vocab = [w.strip() for w in open('vocab.txt').readlines()]
-for w,v in zip(vocab, m):
-f = open('embeddings.txt', 'w')
+
+model_path = '../models/skip_thoughts_uni_2017_02_02'
+
+m = np.load(os.path.join(model_path, 'embeddings.npy'))
+vocab = [w.strip() for w in open(os.path.join(model_path, 'vocab.txt')).readlines()]
+
+f = open(os.path.join(model_path, 'embeddings.txt'), 'w')
 for w,v in zip(vocab, m):
     v = ' '.join(['%.4f'%float(num) for num in v])
     f.write(w + ' ' + v + '\n')
